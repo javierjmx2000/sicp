7dba11e26b356d6c719714ebadcfa3e6ca7462c6
Javier J <themjnunez+1@gmail.com>
2019-09-17 17:33:09 -0500

    Sep 17
    - Exercise 2.1

--- 2019_09.scm
+++ 2019_09.scm
@@ -497,3 +497,32 @@
 (define (equal-rat? x y)
   (= (* (numer x) (denom y))
      (* (numer y) (denom x))))
+
+(define (make-rat n d) (cons n d))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+
+;;;; exercises
+
+;;;;; 2.1
+
+(define (make-rat n d)
+  (let ((g (abs (gcd n d))))
+    (if (positive? d)
+        (cons (/ n g) (/ d g))
+        (cons (- (/ n g)) (- (/ d g))))))
+
+;;;; 2.1.2 abstraction barriers
+
