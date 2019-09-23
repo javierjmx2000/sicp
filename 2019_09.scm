701d15c25f4f07bbecfbde4edb518363afcaa20b
Javier J <themjnunez+1@gmail.com>
2019-09-23 18:21:30 -0500

    Exercise 2.2

--- 2019_09.scm
+++ 2019_09.scm
@@ -526,3 +526,40 @@
 
 ;;;; 2.1.2 abstraction barriers
 
+(define (make-rat n d)
+  (cons n d))
+(define (numer x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (car x) g)))
+(define (denom x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (cdr x) g)))
+
+;;;; exercises
+
+;;;;; 2.2
+
+(define (make-segment start end)
+  (cons start end))
+(define (start-segment segment)
+  (car segment))
+(define (end-segment segment)
+  (cdr segment))
+(define (make-point x y)
+  (cons x y))
+(define (x-point point)
+  (car point))
+(define (y-point point)
+  (cdr point))
+(define (midpoint-segment segment)
+  (make-point (average (x-point (start-segment segment))
+                       (x-point (end-segment segment)))
+              (average (y-point (start-segment segment))
+                       (y-point (end-segment segment)))))
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ", ")
+  (display (y-point p))
+  (display ")"))
