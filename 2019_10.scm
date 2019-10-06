a62c9bbdf77d67568a44b1ad491f8348ebca7d51
Javier J <themjnunez+1@gmail.com>
2019-10-06 15:39:14 -0500

    Exercise 2.8

--- 2019_10.scm
+++ 2019_10.scm
@@ -12,7 +12,17 @@
             (/ x n))
       count))
 
+
 ;; 2.7
 
 (define (upper-bound x) (car x))
 (define (lower-bound x) (cdr x))
+
+
+;; 2.8
+
+(define (sub-internal x y)
+  (make-interval (- (lower-bound y) (lower-bound x))
+                 (- (upper-bound y) (upper-bound x))))
+
+(define (make-interval a b) (cons a b))
