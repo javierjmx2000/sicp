ab54ba683a07f7a80a4b91f6ee9dfd444ffc4679
Javier J <themjnunez+1@gmail.com>
2019-10-08 11:27:46 -0500

    Exercise 2.19

--- 2019_10.scm
+++ 2019_10.scm
@@ -79,3 +79,23 @@
       l
       (append (reverse (cdr l))
               (list (car l)))))
+
+
+;; 2.19
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+(define (no-more? coin-values)
+  (= (length coin-values) 0))
+
+(define except-first-denomination cdr)
+
+(define first-denomination car)
