c6f6abeef5f64addcf2a8b38060c82ea6c067185
Javier J <themjnunez+1@gmail.com>
2019-10-10 11:34:27 -0500

    Exercise 2.30

--- 2019_10.scm
+++ 2019_10.scm
@@ -199,3 +199,19 @@
             (branch-torque r))
          (branch-balanced? l)
          (branch-balanced? r))))
+
+
+;; 2.30
+
+(define (square-tree tree)
+  (cond ((null? tree) (list))
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree sub-tree)
+             (square sub-tree)))
+       tree))
