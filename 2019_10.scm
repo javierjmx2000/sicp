2b29596fc2b05cecbfd6b8d53322286a544444ae
Javier J <themjnunez+1@gmail.com>
2019-10-25 11:47:53 -0500

    Exercise 2.60

--- 2019_10.scm
+++ 2019_10.scm
@@ -493,3 +493,27 @@
   (cond ((null? set) false)
         ((equal? x (car set)) true)
         (else (element-of-set? x (cdr set)))))
+
+
+;; 2.60
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= (car set) x) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define adjoin-set cons)
+
+(define (union-set s1 s2)
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        (else (adjoin-set (car s1)
+                          (union-set (cdr s1) s2)))))
+
+(define (intersection-set s1 s2)
+  (cond ((null? s1) '())
+        ((null? s2) '())
+        ((element-of-set? (car s1) s2)
+         (adjoin-set (car s1)
+                     (intersection-set (cdr s1) s2)))
+        (else (intersection-set (cdr s1) s2))))
