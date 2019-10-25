ec6f40d1babe487a64123a0a1d1301dac490f553
Javier J <themjnunez+1@gmail.com>
2019-10-25 11:34:09 -0500

    Exercise 2.58

--- 2019_10.scm
+++ 2019_10.scm
@@ -437,9 +437,6 @@
         ((and (number? a1) (number? a2)) (+ a1 a2))
         (else (list '+ a1 a2))))
 
-(define (sum? x)
-  (and (pair? x) (eq? (car x) '+)))
-
 (define (addend s) (cadr s))
 
 (define (augend s) (caddr s))
@@ -464,3 +461,20 @@
       (fold-right make-product
                   1
                   (cddr p))))
+
+
+;; 2.58
+
+(define (sum? exp)
+  (and (pair? exp) (eq? (cadr exp) '+)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define addend car)
+
+(define augend caddr)
+
