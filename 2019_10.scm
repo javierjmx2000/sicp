0a5194b68345f1e071155ba5d467ee51d2bc87b8
Javier J <themjnunez+1@gmail.com>
2019-10-28 11:06:18 -0600

    Exercise 2.62

--- 2019_10.scm
+++ 2019_10.scm
@@ -527,3 +527,16 @@
         ((> x (car set))
          (cons (car set) (adjoin-set x (cdr set))))
         (else set)))
+
+
+;; 2.62
+
+(define (union-set s1 s2)
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        ((< (car s1) (car s2))
+         (cons (car s1) (union-set (cdr s1) s2)))
+        ((> (car s1) (car s2))
+         (cons (car s2) (union-set s1 (cdr s2))))
+        (else
+         (cons (car s1) (union-set (cdr s1) (cdr s2))))))
